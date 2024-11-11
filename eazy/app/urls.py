dba98bc5a2ad9483f8b1aa95c4b448409db88621
althafnavas4 <nalthaf13@gmail.com>
2024-11-11 22:40:07 +0530

    new

---
+++ eazy/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.eazy_login),
+    path('home',views.home),
+    path('arms_logout',views.eazy_logout),
+    path('add_prod',views.add_prod),
+    path('edit/<pid>',views.edit),
+    path('delete/<pid>',views.delete)
+
+]
